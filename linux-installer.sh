a90e401f2f1c000e8bd2be36c5d7aa28d25051bc
Bertram Truong <b@bertramtruong.com>
2018-06-14 18:01:20 +1000

    Update to 0.6.0

--- linux-installer.sh
+++ linux-installer.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
-VERSION=${VERSION:-0.5.0}
-RYZEN_VERSION=${RYZEN_VERSION:-0.5.0}
+VERSION=${VERSION:-0.6.0}
+RYZEN_VERSION=${RYZEN_VERSION:-0.6.0}
 WORKING_DIR="nimbuspool-miner"
 
 # List of supported CPU; if not in this list, then
