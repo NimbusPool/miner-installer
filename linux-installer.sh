94678737a9da15dc328fa623fbb1e0b2d51ecaf5
Bertram Truong <b@bertramtruong.com>
2018-07-06 02:33:04 +1000

    Update linux-installer.sh

--- linux-installer.sh
+++ linux-installer.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
-VERSION=${VERSION:-0.7.2}
-RYZEN_VERSION=${RYZEN_VERSION:-0.7.2}
+VERSION=${VERSION:-0.7.3}
+RYZEN_VERSION=${RYZEN_VERSION:-0.7.3}
 WORKING_DIR="nimbuspool-miner"
 
 # List of supported CPU; if not in this list, then
